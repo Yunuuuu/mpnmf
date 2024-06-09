44519ebd46ce29f2d4a9e40ca1038d42d1c45746
yun <yunyunpp96@outlook.com>
2024-06-10 03:04:40 +0800

    add function `mp_heatmap`

---
+++ R/heatmap.R
@@ -0,0 +1,66 @@
+#' Meta program similarity heatmap
+#'
+#' @param x A [mpnmf][mp] object.
+#' @param ... Additional arguments passed to [Heatmap][ComplexHeatmap::Heatmap].
+#' @inheritParams ComplexHeatmap::Heatmap
+#' @param palette A named color define to meta program bar color in top and
+#' left. Can also be a string of palette name in
+#' [RColorBrewer][RColorBrewer::brewer.pal].
+#' @export
+mp_heatmap <- function(
+    x, ...,
+    col = viridisLite::viridis(100, option = "A", direction = -1),
+    palette = NULL, show_row_names = FALSE, show_column_names = FALSE) {
+    assert_s3_class(x, "mpnmf")
+    mat <- attr(x, "similarity")
+    stats <- attr(x, "stats")
+    n <- nlevels(stats$members)
+    if (is.null(palette)) {
+        if (n > 12L) {
+            palette <- ComplexHeatmap:::default_col(stats$members)
+        } else {
+            palette <- RColorBrewer::brewer.pal(n, "Paired")
+            names(palette) <- levels(stats$members)
+        }
+    } else if (rlang::is_string(palette) && is.null(names(palette))) {
+        palette <- match.arg(palette, rownames(RColorBrewer::brewer.pal.info))
+        max <- RColorBrewer::brewer.pal.info[palette, "maxcolors"]
+        if (max < n) {
+            cli::cli_abort(
+                "{palette} is not sufficient to provide {.val {n}} colors"
+            )
+        }
+        palette <- RColorBrewer::brewer.pal(n, palette)
+        names(palette) <- levels(stats$members)
+    }
+    args <- list(
+        matrix = mat, name = "similarity",
+        left_annotation = ComplexHeatmap::HeatmapAnnotation(
+            MP = stats$members, col = list(MP = palette),
+            which = "row", show_legend = FALSE
+        ),
+        top_annotation = ComplexHeatmap::HeatmapAnnotation(
+            MP = stats$members, col = list(MP = palette),
+            which = "column"
+        ),
+        col = col, ...,
+        show_row_names = show_row_names,
+        show_column_names = show_column_names
+    )
+    if (!is.null(stats$tree)) {
+        # restore tree object from `mpnmf` object
+        args$clustering_distance_rows <- function(x) {
+            stats::as.dist(1 - x[, rownames(x)])
+        }
+        args$clustering_distance_columns <- function(x) {
+            stats::as.dist(1 - x[, rownames(x)])
+        }
+        args$clustering_method_rows <- stats$tree$method
+        args$clustering_method_columns <- stats$tree$method
+        args$row_dend_reorder <- TRUE
+        args$column_dend_reorder <- TRUE
+        args$row_split <- stats$members
+        args$column_split <- stats$members
+    }
+    do.call(ComplexHeatmap::Heatmap, args)
+}
