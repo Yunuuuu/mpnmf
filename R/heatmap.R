a2aad9950cda1fdcbc662e6ed7f5a24d68cde7c5
yun <yunyunpp96@outlook.com>
2024-07-08 23:39:07 +0800

    mp_heatmap: add highlight argument

--- R/heatmap.R
+++ R/heatmap.R
@@ -2,16 +2,29 @@
 #'
 #' @param x A [mpnmf][mp] object.
 #' @param ... Additional arguments passed to [Heatmap][ComplexHeatmap::Heatmap].
+#' @param highlight A boolean value indicates whether highlight significant meta
+#' programs or a [gpar][grid::gpar] object define the highlight
+#' [rect][grid::grid.rect].
+#' @inheritParams mpnmf-method
 #' @inheritParams ComplexHeatmap::Heatmap
 #' @param palette A named character define to meta program color bar in top and
 #' left. Can also be a string of palette name in
 #' [RColorBrewer][RColorBrewer::brewer.pal].
 #' @export
 mp_heatmap <- function(
-    x, ...,
+    x, ..., highlight = TRUE, s_min = 1 / 3, layer_fun = NULL,
     col = viridisLite::viridis(100, option = "A", direction = -1),
     palette = NULL, show_row_names = FALSE, show_column_names = FALSE) {
     assert_s3_class(x, "mpnmf")
+    if (isTRUE(highlight)) {
+        highlight <- grid::gpar(lty = 2, lwd = 2, col = "red")
+    } else if (inherits(highlight, "gpar")) {
+        highlight$fill <- NA
+    } else if (isFALSE(highlight)) {
+        cli::cli_abort(
+            "{.arg highlight} must be a boolean value or a {.cls gpar} object"
+        )
+    }
     mat <- attr(x, "similarity")
     stats <- attr(x, "stats")
     n <- nlevels(stats$members)
@@ -33,7 +46,36 @@ mp_heatmap <- function(
         palette <- RColorBrewer::brewer.pal(n, palette)
         names(palette) <- levels(stats$members)
     }
+    if (inherits(highlight, "gpar")) {
+        sig_programs <- mp_programs(x, s_min = s_min)
+        sig_index <- match(levels(stats$members), names(sig_programs))
+        highlight_layer_fun <- function(highlight) {
+            slice_numbers <- strsplit(grid::current.viewport()$name,
+                "_",
+                fixed = TRUE
+            )[[1L]]
+            slice_numbers <- as.integer(utils::tail(slice_numbers, n = 2L))
+            if (any(.subset(slice_numbers, 1L) == sig_index) &&
+                # the second and third items are the slice row and column number
+                .subset(slice_numbers, 1L) == .subset(slice_numbers, 2L)) {
+                grid::grid.rect(gp = highlight)
+            }
+        }
+        if (is.null(layer_fun)) {
+            internal_layer_fun <- function(j, i, x, y, w, h, fill) {
+                highlight_layer_fun(highlight)
+            }
+        } else {
+            internal_layer_fun <- function(j, i, x, y, w, h, fill) {
+                layer_fun(j, i, x, y, w, h, fill)
+                highlight_layer_fun(highlight)
+            }
+        }
+    } else {
+        internal_layer_fun <- layer_fun
+    }
     dist_fn <- function(x) stats::as.dist(1 - x[, rownames(x)])
+
     args <- list(
         matrix = mat,
         left_annotation = ComplexHeatmap::HeatmapAnnotation(
@@ -48,6 +90,7 @@ mp_heatmap <- function(
             annotation_legend_param = list(title = NULL)
         ),
         col = col, ...,
+        layer_fun = internal_layer_fun,
         row_split = stats$members,
         column_split = stats$members,
         clustering_distance_rows = dist_fn,
